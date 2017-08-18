09179f173cfaf17171744b3691cddc09a89e3601
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:35:47 +0200

    sepolicy: Initial repo structure
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Potato
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/cosmic/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Potato
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/cosmic/sepolicy/qcom
