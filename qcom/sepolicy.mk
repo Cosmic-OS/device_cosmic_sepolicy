f0ba876ffb004aedf9dd0b7b4169d39190b2ee3b
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-30 00:04:20 -0400

    sepolicy: Include platform specific sepolicy
    Change-Id: Ib2c58c95f934477e782fec64d2aa4dc951d69b9d

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,7 +1,8 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Potato
+# that inherit from cosmic
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/cosmic/sepolicy/qcom
+    device/cosmic/sepolicy/qcom/common \
+    device/cosmic/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
